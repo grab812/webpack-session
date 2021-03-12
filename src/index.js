4c6453ad7a21e7ec65cc2533682fc67233eecb67
rameee <2velyram@gmail.com>
2021-03-12 17:05:27 +0900

    typing letter html&css setting

--- src/index.js
+++ src/index.js
@@ -4,5 +4,16 @@ import syaku from './images/syaku.png';
 
 const app = document.getElementById('app');
 
-app.innerHTML = '<img src="' + webpack + '" />';
-app.innerHTML += '<br/><img src="' + syaku + '" />';
+// app.innerHTML = '<img src="' + webpack + '" />';
+// app.innerHTML += '<br/><img src="' + syaku + '" />';
+
+app.innerHTML = `<div class="main-section">
+<!-- letter area -->
+<div class="write-form">
+    <div class="textbox_area">
+        <div class="textbox_area__temp js-textbox_area__temp" contentEditable="true" data-placeholder="Ex)&#13; 내용은 최대 공백 포함 50자,&#13;&#10; 5줄까지 입력 가능합니다.">
+        </div>    
+    </div>
+</div>
+<!-- //letter area -->
+</div>`;
